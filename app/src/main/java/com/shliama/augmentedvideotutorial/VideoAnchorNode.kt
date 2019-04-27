3e0d84e2ca92d6d83f62b71bebed951353c69768
Oleksii Shliama <o.shliama@gmail.com>
2019-04-27 16:47:08 +0800

    Step 2 - Using child node as the actual video renderer for the sake of better composition

--- app/src/main/java/com/shliama/augmentedvideotutorial/VideoAnchorNode.kt
+++ app/src/main/java/com/shliama/augmentedvideotutorial/VideoAnchorNode.kt
@@ -1,11 +1,19 @@
 package com.shliama.augmentedvideotutorial
 
 import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.Node
 import com.google.ar.sceneform.math.Quaternion
 import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.Renderable
 import com.shliama.augmentedvideotutorial.VideoScaleType.*
 
 class VideoAnchorNode : AnchorNode() {
+
+    private val videoNode = Node().also { it.setParent(this) }
+
+    override fun setRenderable(renderable: Renderable?) {
+        videoNode.renderable = renderable
+    }
 
     fun setVideoProperties(
         videoWidth: Float, videoHeight: Float, videoRotation: Float,
@@ -21,7 +29,7 @@ class VideoAnchorNode : AnchorNode() {
         imageWidth: Float, imageHeight: Float,
         videoScaleType: VideoScaleType
     ) {
-        when (videoScaleType) {
+        videoNode.localScale = when (videoScaleType) {
             FitXY -> scaleFitXY(imageWidth, imageHeight)
             CenterCrop -> scaleCenterCrop(videoWidth, videoHeight, imageWidth, imageHeight)
             CenterInside -> scaleCenterInside(videoWidth, videoHeight, imageWidth, imageHeight)
@@ -29,19 +37,17 @@ class VideoAnchorNode : AnchorNode() {
     }
 
     private fun rotateNode(videoRotation: Float) {
-        localRotation = Quaternion.axisAngle(Vector3(0.0f, -1.0f, 0.0f), videoRotation)
+        videoNode.localRotation = Quaternion.axisAngle(Vector3(0.0f, -1.0f, 0.0f), videoRotation)
     }
 
-    private fun scaleFitXY(imageWidth: Float, imageHeight: Float) {
-        localScale = Vector3(imageWidth, 1.0f, imageHeight)
-    }
+    private fun scaleFitXY(imageWidth: Float, imageHeight: Float) = Vector3(imageWidth, 1.0f, imageHeight)
 
-    private fun scaleCenterCrop(videoWidth: Float, videoHeight: Float, imageWidth: Float, imageHeight: Float) {
+    private fun scaleCenterCrop(videoWidth: Float, videoHeight: Float, imageWidth: Float, imageHeight: Float): Vector3 {
         val isVideoVertical = videoHeight > videoWidth
         val videoAspectRatio = if (isVideoVertical) videoHeight / videoWidth else videoWidth / videoHeight
         val imageAspectRatio = if (isVideoVertical) imageHeight / imageWidth else imageWidth / imageHeight
 
-        localScale = if (isVideoVertical) {
+        return if (isVideoVertical) {
             if (videoAspectRatio > imageAspectRatio) {
                 Vector3(imageWidth, 1.0f, imageWidth * videoAspectRatio)
             } else {
@@ -56,12 +62,12 @@ class VideoAnchorNode : AnchorNode() {
         }
     }
 
-    private fun scaleCenterInside(videoWidth: Float, videoHeight: Float, imageWidth: Float, imageHeight: Float) {
+    private fun scaleCenterInside(videoWidth: Float, videoHeight: Float, imageWidth: Float, imageHeight: Float): Vector3 {
         val isVideoVertical = videoHeight > videoWidth
         val videoAspectRatio = if (isVideoVertical) videoHeight / videoWidth else videoWidth / videoHeight
         val imageAspectRatio = if (isVideoVertical) imageHeight / imageWidth else imageWidth / imageHeight
 
-        localScale = if (isVideoVertical) {
+        return if (isVideoVertical) {
             if (videoAspectRatio < imageAspectRatio) {
                 Vector3(imageWidth, 1.0f, imageWidth * videoAspectRatio)
             } else {
